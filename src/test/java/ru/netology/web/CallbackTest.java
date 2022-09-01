d42e28bc99ec6a859955ec7ad32c9a60b177f85c
Maria Bychkova <mar.byckova@gmail.com>
2022-09-01 12:09:29 +0300

    Changed Selenium version

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,4 +1,23 @@
 package ru.netology.web;
 
-public class CallbackTest {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CallbackTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void SetUp(){
+        driver = new ChromeDriver();
+    }
 }
