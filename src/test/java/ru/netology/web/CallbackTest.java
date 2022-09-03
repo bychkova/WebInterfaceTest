96f96f022fdb315ab6ee6d3e378d428c152c37f2
Maria Bychkova <mar.byckova@gmail.com>
2022-09-03 14:11:09 +0300

    Test template

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,23 +1,49 @@
 package ru.netology.web;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CallbackTest {
-
     private WebDriver driver;
-
     @BeforeAll
     public void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver.exe");
     }
-
     @BeforeEach
     void SetUp(){
         driver = new ChromeDriver();
     }
+    @AfterEach
+    public void Close(){
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void Test(){
+        driver.get("http://localhost:9999");
+
+        List<WebElement> inputs = driver.findElements(By.className("input__control"));
+
+        inputs.get(0).sendKeys("Михаил");
+        inputs.get(1).sendKeys("+79291104279");
+        driver.findElement(By.className("checkbox__box")).click(); //agreement
+        driver.findElement(By.tagName("button")).click(); //submit
+        String actual = driver.findElement(By.className("paragraph")).getText(); //success text
+        //String actuale = driver.findElement(By.()).getText(); //success text
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+
+        assertEquals(expected, actual);
+    }
+
 }
